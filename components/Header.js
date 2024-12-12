f725621da22bd63edc787c7046fb0dfc2da1eddd
Sunil Gupta <sunilk98850@gmail.com>
2024-12-12 23:06:45 +0530

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white py-4">
+      <h1 className="text-3xl font-bold text-center">Anime Stream</h1>
+    </header>
+  );
+}
